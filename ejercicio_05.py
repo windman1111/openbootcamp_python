9c90508da2a75043c25bb80a75aaae38a5c468bf
windman1111 <123778061+windman1111@users.noreply.github.com>
2023-03-17 15:37:13 +0100

    Update ejercicio_05.py

--- ejercicio_05.py
+++ ejercicio_05.py
@@ -1,10 +1,13 @@
 bisiesto = int(input("Introduce un año y te diremos si es bisiesto: \n"))
 
 def calcular_bisiesto(bisiesto):
-    calc_bisi = bisiesto % 4
-    if calc_bisi == 0:
-        print(f"El año {bisiesto} es bisiesto.")
-    else:
-        print(f"El año {bisiesto} no es bisiesto")
+    return bisiesto % 4
+    
+calc_bisi = calcular_bisiesto(bisiesto)
+
+if calc_bisi == 0:
+    print(f"El año {bisiesto} es bisiesto.")
+else:
+    print(f"El año {bisiesto} no es bisiesto")
 
 calcular_bisiesto(bisiesto)
